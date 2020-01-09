297e586c3887291b7d635ec193a3236158f3d3f4
Willian Rocha <willian.rocha@viaflow.com.br>
2020-01-09 09:24:35 -0300

    azurepass

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,9 @@
 const express = require('express');
 const routes = require('../src/routes');
-const morgan = require('morgan');
 const mongoose = require('mongoose');
-require('firebase/storage');
 
-const firebaseConfig = {
-    apiKey: "api-key",
-    authDomain: "project-id.firebaseapp.com",
-    databaseURL: "https://project-id.firebaseio.com",
-    projectId: "project-id",
-    storageBucket: "project-id.appspot.com",
-    messagingSenderId: "sender-id",
-    appID: "app-id",
-}
 
-firebase.initializeApp(firebaseConfig);
+
 
 
 const app = express();
@@ -26,7 +15,7 @@ mongoose.connect('mongodb+srv://showroomcasas:bDHe1lxMHme8hd4Q@showroomcasas-drp
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+
 app.use(routes);
 
 app.listen(3333);
